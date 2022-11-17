01767a5c2a258cf973b5ad7540f70b4b2e4ae15a
mail@markberk.nl <mail@markberk.nl>
2022-11-17 10:06:02 +0100

    aanpassing in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-//Include Configuration File
+//Include Configuration File(database instellingen / eigen functies die je op meerdere paginas wil kunnen gebruiken kunnen hierin
 include('config.php');
 ?>
 
@@ -10,21 +10,23 @@ include('config.php');
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Marnix</title>
   <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
-<!-- CSS only -->
+<!-- CSS van bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-<!-- JavaScript Bundle with Popper -->
+<!-- JavaScript Bundle with Popper van bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
  </head>
  <body class='bg-light container-fluid' height=100% width=100%>
  <div class=container>
  <?php
+ // menu includen (hier een require gebruikt, als deze niet bestaat gaat de pagina stuk)
  	require("menu-top.php");
-
+// als er een ?p=123 achter de url staat, dan gaat hij die pagina met 123.php ophalen en laten zien
 	if(isset($_GET['p']))
 		{
 			$page = $_GET['p'];
 		}
 		else{
+			//als er geen ?p= achter de url staat, dan pakt hij de home pagina
 			$page='home';
 		}
 	
